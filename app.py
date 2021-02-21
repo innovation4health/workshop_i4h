7c00cacce7f93b88a36f4b6694cb660d1e2b54d6
Rodrigo <rodrigolima82@gmail.com>
2021-02-21 11:27:11 -0300

    if

--- app.py
+++ app.py
@@ -57,63 +57,64 @@ def main():
             else:
                 sinais = loadmat('fe_heart_sensor/dados/paciente_rodrigo.mat')
 
-            sinais_mat = sinais['val']
+            if sinais:
+                sinais_mat = sinais['val']
 
-            # Extraindo o Batimento Cardiaco do Paciente
-            for channelid, channel in enumerate(sinais_mat):
-                resultado = ecg.ecg(signal = channel, sampling_rate = 300, show = False)
-                heart_rate = np.zeros_like(channel, dtype = 'float')
-                heart_rate = resultado['heart_rate']    
-                
-                st.write('BPM máximo: ', max(heart_rate))  
-                
-                try:
-                    if max(heart_rate) > 130:
-                        HR = 1
-                    else:
-                        HR = 0
-                except:
-                    continue    
+                # Extraindo o Batimento Cardiaco do Paciente
+                for channelid, channel in enumerate(sinais_mat):
+                    resultado = ecg.ecg(signal = channel, sampling_rate = 300, show = False)
+                    heart_rate = np.zeros_like(channel, dtype = 'float')
+                    heart_rate = resultado['heart_rate']    
+                    
+                    st.write('BPM máximo: ', max(heart_rate))  
+                    
+                    try:
+                        if max(heart_rate) > 130:
+                            HR = 1
+                        else:
+                            HR = 0
+                    except:
+                        continue    
 
-            # Fazendo a previsao
-            # Carregamos o modelo   
-            modelo = load_model('fe_heart_sensor/model/ResNet_30s_34lay_16conv.hdf5')
+                # Fazendo a previsao
+                # Carregamos o modelo   
+                modelo = load_model('fe_heart_sensor/model/ResNet_30s_34lay_16conv.hdf5')
 
-            # Valores constantes
-            frequencia = 300
-            tamanho_janela = 30 * frequencia
+                # Valores constantes
+                frequencia = 300
+                tamanho_janela = 30 * frequencia
 
-            # Fazendo a previsao
-            x = processamento(sinais_mat, tamanho_janela)
+                # Fazendo a previsao
+                x = processamento(sinais_mat, tamanho_janela)
 
-            # Previsões com o modelo (retorna as probabilidades)
-            prob_x, ann_x = previsoes(modelo, x)
+                # Previsões com o modelo (retorna as probabilidades)
+                prob_x, ann_x = previsoes(modelo, x)
 
-            # Lista de classes
-            #classes = ['A', 'N', 'O', '~']
+                # Lista de classes
+                #classes = ['A', 'N', 'O', '~']
 
-            x = processamento(sinais_mat, tamanho_janela)
-            prob_x, ann_x = previsoes(modelo, x)
-            st.write('Probabilidade FA: ', prob_x[0, 0])
+                x = processamento(sinais_mat, tamanho_janela)
+                prob_x, ann_x = previsoes(modelo, x)
+                st.write('Probabilidade FA: ', prob_x[0, 0])
 
-            # Dataframe para o risco estratificado
-            df_risco = pd.DataFrame({'Probabilidade':[prob_x[0, 0]], 'HR':HR})
-            df_risco['Risco'] = df_risco.apply(classifica_risco, axis = 1)
-            st.write('Risco: ', df_risco['Risco'][0])
+                # Dataframe para o risco estratificado
+                df_risco = pd.DataFrame({'Probabilidade':[prob_x[0, 0]], 'HR':HR})
+                df_risco['Risco'] = df_risco.apply(classifica_risco, axis = 1)
+                st.write('Risco: ', df_risco['Risco'][0])
 
-            # Plot
-            x_axis = np.linspace(0., float(len(sinais_mat[0]) / 300), num = len(sinais_mat[0]))
-            plt.rcParams.update({'font.size': 14})
-            fig, ax = plt.subplots(figsize = (16,5))
+                # Plot
+                x_axis = np.linspace(0., float(len(sinais_mat[0]) / 300), num = len(sinais_mat[0]))
+                plt.rcParams.update({'font.size': 14})
+                fig, ax = plt.subplots(figsize = (16,5))
 
-            ax.plot(x_axis, sinais_mat[0], 'magenta')
-            ax.axis([0, len(sinais_mat[0]) / 300, -2200, 2200])
+                ax.plot(x_axis, sinais_mat[0], 'magenta')
+                ax.axis([0, len(sinais_mat[0]) / 300, -2200, 2200])
 
-            ax.set_title('ECG Paciente')
-            ax.set_xlabel("Tempo (em segundos)")
-            ax.set_ylabel("Milli Volts")
+                ax.set_title('ECG Paciente')
+                ax.set_xlabel("Tempo (em segundos)")
+                ax.set_ylabel("Milli Volts")
 
-            st.write(fig)
+                st.write(fig)
     
     if choice == 'Sobre':
         st.markdown("### Who I am")
